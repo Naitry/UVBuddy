dbb24d372371e4514a8d3f9cef4d0e72d0fe64d4
G.L. Flores <124198528+grlefl@users.noreply.github.com>
2024-04-30 09:50:41 -0500

    rough draft of UV funciontality

--- uvbuddy/uvbuddy.cpp
+++ uvbuddy/uvbuddy.cpp
@@ -1,15 +1,16 @@
-#include "uvb_logger.h"
+#include "uvbuddy.h"
 
 void setup() {
   Serial.begin(9600); // ???
 
   // Initialize I/O
   pinMode(UV_SENSE, INPUT)      // uv sensor as input 
-  pinMode(SENSE_EN, OUTPUT)     // sensor enable as output 
+  pinMode(SENSE_EN, OUTPUT)     // uv sensor enable as output 
   // initialize LEDs 
 
   // Initialize states 
-  digitalWrite(SENSE_EN, HIGH)  // set sensor enable high 
+  digitalWrite(SENSE_EN, HIGH)  // set uv sensor enable high 
+
   // initialize LEDs 
 
   // Initialize SD card
@@ -37,12 +38,21 @@ void loop() {
   Serial.println("UV Index: " + String(uvIndex)); // debugging 
 }
 
-// int readUVIndex() {
-//   int uvValue = analogRead(UV_SENSE_PIN);
-//   // Convert analog reading to UV index (you'll need to calibrate this based on your sensor's specifications)
-//   int uvIndex = map(uvValue, 0, 1023, 0, 15);
-//   return uvIndex;
-// }
+// https://learn.sparkfun.com/tutorials/ml8511-uv-sensor-hookup-guide/all
+int getUVIndex() {
+  int uvValue = analogRead(UV_SENSE);
+  int refLevel = analogRead(3V3)        // analog read? I/O?
+  uvVoltage / uvValue = 3.3 / refLevel` // solves? 
+  float uvIntensity = mapfloat(uvVoltage, 0.99, 2.8, 0.0, 15.0); // convert the voltage to a UV intensity level
+  return uvIntensity 
+}
+
+//The Arduino Map function but for floats
+//From: http://forum.arduino.cc/index.php?topic=3922.0
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
 
 void logData(int uvIndex) {
   String dataString = "timestamp_placeholder," + String(uvIndex);
